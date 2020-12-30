c817deaa51bb1cfec552a6b8e541630dc6eec6bf
Stephen Chu <chu.stephen15@gmail.com>
2020-12-29 18:04:45 -0800

    calculate average weekly drinking for male

--- Student_Alcohol_Consumption/ETL_queries.sql
+++ Student_Alcohol_Consumption/ETL_queries.sql
@@ -1,3 +1,7 @@
 -- Calculate average drinking throughout the week for female students
 SELECT ROUND(AVG(workday_drinking), 2) AS "Average Workday Drinking", ROUND(AVG(weekend_drinking), 2) AS "Average Weekend Drinking"
 FROM female_study_habits;
+
+-- Calculate average drinking throughout the week for male students
+SELECT ROUND(AVG(workday_drinking), 2) AS "Average Workday Drinking", ROUND(AVG(weekend_drinking), 2) AS "Average Weekend Drinking"
+FROM male_study_habits;
