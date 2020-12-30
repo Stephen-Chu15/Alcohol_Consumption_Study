93decf9d5f7021ede0afe3c8ba1f1e1d67596b83
Stephen Chu <chu.stephen15@gmail.com>
2020-12-29 18:05:10 -0800

    calculate the average amount of absences for females by age

--- Student_Alcohol_Consumption/ETL_queries.sql
+++ Student_Alcohol_Consumption/ETL_queries.sql
@@ -5,3 +5,9 @@ FROM female_study_habits;
 -- Calculate average drinking throughout the week for male students
 SELECT ROUND(AVG(workday_drinking), 2) AS "Average Workday Drinking", ROUND(AVG(weekend_drinking), 2) AS "Average Weekend Drinking"
 FROM male_study_habits;
+
+-- Calculate average absences for females by age
+SELECT age, COUNT(school) AS "Total Students", ROUND(AVG(absences) , 2) AS "Average Absences"
+FROM female_study_habits
+GROUP BY age
+ORDER BY age DESC;
